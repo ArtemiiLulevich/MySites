77219b91427d146b8fd55d66b02facb16c5b504d
Artemii <artemii.lulevich@gmail.com>
2020-05-29 19:30:53 +0300

    Added to admin site: 1) Search field; 2) filter etc.

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -4,5 +4,14 @@ from .models import Post
 
 
 # Simple admin page without customization.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    # added search by author. 'ForeignKey__related_fieldname'(in Base)
+    search_fields = ('title', 'body', 'author__username')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
 
