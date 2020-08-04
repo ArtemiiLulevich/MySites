a0ddd8534e0b2e28e59caae1fbe10a95a0c064a9
Artemii <artemii.lulevich@gmail.com>
2020-08-04 20:08:08 +0300

    Add custom template tags func.

---
+++ my_site/blog/templatetags/blog_tags.py
@@ -0,0 +1,26 @@
+from django import template
+from django.db.models import Count
+from ..models import Post
+
+register = template.Library()
+
+
+@register.simple_tag
+def total_posts():
+    """Django will use the function's name as the
+    tag name. If you want to register it using a different name, you can
+    do it by specifying a name attribute, such as @register.simple_tag(name='my_tag')."""
+    return Post.published.count()
+
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(
+        total_comments=Count('comments')
+    ).order_by('-total_comments')[:count]
